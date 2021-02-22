a81bd0e70b197a060d7cd13d128037e5a48229fd
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-22 14:16:14 -0800

    calculate how much silent padding to add to the tick sound

--- test.py
+++ test.py
@@ -1,4 +1,12 @@
 import ffmpeg
+
+BPM = 60
+# 60 BPM is one beat per second, or 1 Hz
+hertz=BPM/60
+tick_sound = ffmpeg.input(tick_filename)
+actual_tick_duration = float(tick_sound.probe()['streams'][0]['duration'])
+
+padding_needed_per_tick = hertz - actual_tick_duration
 input_video = ffmpeg.input('input.gif')
 input_audio = ffmpeg.input('input.wav')
 input_audio2 = ffmpeg.input('clap.mp3')
