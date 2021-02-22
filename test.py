e12320a257bbb338bf5748630f05d15bb2fa91fe
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-22 12:29:17 -0800

    add fast clap (tapping on a glass) to demonstrate adding two metronomes of different frequencies

--- test.py
+++ test.py
@@ -2,8 +2,11 @@ import ffmpeg
 input_video = ffmpeg.input('input.gif')
 input_audio = ffmpeg.input('input.wav')
 input_audio2 = ffmpeg.input('clap.mp3')
+input_audio3 = ffmpeg.input('clapfast.mp3')
 
-merged_audio = ffmpeg.filter([input_audio, input_audio2], 'amix')
+claps = ffmpeg.filter([input_audio2, input_audio3], 'amix')
+
+merged_audio = ffmpeg.filter([input_audio, claps], 'amix')
 
 ffmpeg.concat(
 	input_video,
