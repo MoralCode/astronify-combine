6dd007b16b495e900d6db3b15173f7d336fc48ee
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-22 12:23:50 -0800

    add clap track

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import ffmpeg
 input_video = ffmpeg.input('input.gif')
 input_audio = ffmpeg.input('input.wav')
+input_audio2 = ffmpeg.input('clap.mp3')
 
 
 ffmpeg.concat(input_video, input_audio, v=1, a=1).output('./output.mp4').run()
