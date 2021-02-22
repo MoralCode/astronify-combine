e77dda79f08bcd568cc5356d961a4bac75b46537
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-22 12:24:55 -0800

    merge audio

--- test.py
+++ test.py
@@ -3,5 +3,6 @@ input_video = ffmpeg.input('input.gif')
 input_audio = ffmpeg.input('input.wav')
 input_audio2 = ffmpeg.input('clap.mp3')
 
+merged_audio = ffmpeg.filter([input_audio, input_audio2], 'amix')
 
 ffmpeg.concat(input_video, input_audio, v=1, a=1).output('./output.mp4').run()
