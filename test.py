b03828d55520fccf8993ad743ce564d391d2dfff
Adrian Edwards <17362949+MoralCode@users.noreply.github.com>
2021-02-22 20:53:34 -0800

    make it adjust the length of ticks to allow for adjustible frequency

--- test.py
+++ test.py
@@ -1,20 +1,38 @@
 import ffmpeg
+from shutil import copyfile
+from pydub import AudioSegment
 
 BPM = 60
-# 60 BPM is one beat per second, or 1 Hz
-hertz=BPM/60
-tick_sound = ffmpeg.input(tick_filename)
-actual_tick_duration = float(tick_sound.probe()['streams'][0]['duration'])
 
-padding_needed_per_tick = hertz - actual_tick_duration
+def bpm_to_time_per_tick(bpm):
+	return (1/bpm)/60
+ 
+
+def calculate_padding_needed_ms(target_sec, actual_sec):
+	return int((target_sec - actual_sec)*1000)
+
+
+def process_tick(bpm, filename):
+	tick = AudioSegment.from_wav(filename)
+	pad = calculate_padding_needed_ms(bpm_to_time_per_tick(bpm), tick.duration_seconds)
+	padding = AudioSegment.silent(duration=pad)
+	final_tick = tick + padding
+	final_tick.export(filename, format="wav")
+
+tick_filename='oneclap-megasqueeze'
+# make a copy of the source tick noise so it doesnt get overwritten
+#TODO: check if a file with the same name exists so as to prevent creating a new one
+newname = tick_filename + "." + str(BPM) + ".wav"
+copyfile(tick_filename +".wav", newname)
+
+process_tick(BPM, newname)
+
 input_video = ffmpeg.input('input.gif')
 input_audio = ffmpeg.input('input.wav')
-input_audio2 = ffmpeg.input('clap.mp3')
-input_audio3 = ffmpeg.input('clapfast.mp3')
+ticks = ffmpeg.input(newname, stream_loop=-1)
 
-claps = ffmpeg.filter([input_audio2, input_audio3], 'amix')
-
-merged_audio = ffmpeg.filter([input_audio, claps], 'amix')
+#merge the input audio with a forever-looping tempo at the provided frequency
+merged_audio = ffmpeg.filter([input_audio, ticks], 'amix', duration="shortest")
 
 ffmpeg.concat(
 	input_video,
